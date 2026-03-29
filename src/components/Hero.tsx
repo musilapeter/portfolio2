cb1724a4c057ee4d8e9028eda2d67dcfd462906c
Dash <95099676+peter-kiilu@users.noreply.github.com>
2026-03-29 10:23:48 +0300

    specs

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,17 +4,17 @@ const Hero: React.FC = () => {
   return (
     <section id="hero" className="w-full">
       <div>
-        <p className="text-primary font-mono mb-5 text-sm lg:text-base">Hi, my name is</p>
+        <p className="text-primary font-mono mb-5 text-sm lg:text-base">Hello, my name is</p>
         <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white mb-4 leading-tight">
           Musila Peter.
         </h1>
         <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-slate mb-6 leading-tight">
-          I build things for the web.
+          I build and Ship.
         </h2>
         <p className="text-slate text-base lg:text-lg mb-8 lg:mb-10 max-w-md">
-          I'm a software engineer specializing in building (and occasionally designing) 
+          I am a MERN Stack software engineer specializing in building (and occasionally designing) 
           exceptional digital experiences. Currently, I'm focused on building accessible, 
-          human-centered products.
+          human-centered AI-powered products.
         </p>
         <a
           href="#projects"
